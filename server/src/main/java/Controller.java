5cde570e19465a0fde7278a039e53c3f1aef528c
James <jflemi9@uic.edu>
2022-04-06 14:36:14 -0400

    set up server as MVC design pattern

---
+++ server/src/main/java/Controller.java
@@ -0,0 +1,33 @@
+
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+
+public class Controller implements Initializable {
+	
+	@FXML
+	private VBox root;
+    
+    @FXML
+    private TextField textField;
+    
+    @FXML
+    private TextField putText;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+        
+    }
+}
--- server/src/main/java/JavaFXTemplate.java
+++ server/src/main/java/JavaFXTemplate.java
@@ -1,29 +1,37 @@
 import javafx.application.Application;
-
-import javafx.scene.Scene;
-
-import javafx.scene.layout.VBox;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 
 public class JavaFXTemplate extends Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		launch(args);
 	}
 
-	//feel free to remove the starter code from this method
 	@Override
-	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
-		primaryStage.setTitle("Welcome to JavaFX");
-		
-		
-		
-				
-		Scene scene = new Scene(new VBox(), 700,700);
-		primaryStage.setScene(scene);
-		primaryStage.show();
+	public void start(Stage primaryStage) {
+		 try {
+	            // Read file fxml and draw interface.
+	            Parent root = FXMLLoader.load(getClass()
+	                    .getResource("/FXML/MyFxml.fxml"));
+	 
+	            primaryStage.setTitle("My Application");
+             Scene s1 = new Scene(root, 500,500);
+            //  s1.getStylesheets().add("/styles/style1.css");
+	            primaryStage.setScene(s1);
+	            primaryStage.show();
+	         
+	        } catch(Exception e) {
+	            e.printStackTrace();
+	            System.exit(1);
+	        }
 	}
 
 }
+
+
+
+
