f0220ee6d200fae3256dcc558d5787f46b33f1bc
James <jflemi9@uic.edu>
2022-04-06 21:13:00 -0400

    create client gui

---
+++ client/src/main/java/Controller.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+
+public class Controller implements Initializable {
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) { }
+
+}
---
+++ client/src/main/java/Main.java
@@ -0,0 +1,34 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+public class Main extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("/FXML/clientPage.fxml"));
+		
+			primaryStage.setTitle("Client");
+			Scene s1 = new Scene(root, 700,400);
+			s1.getStylesheets().add("/styles/style.css");
+			primaryStage.setScene(s1);
+			primaryStage.show();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+}
+
+
+
+
